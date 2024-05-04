96e7ddc05527f310c4f0b2334803240c9b17a7cf
Slavina Stefanova <sstefanova@wikimedia.org>
2024-05-04 21:07:13 +0300

    config

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,160 +12,176 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 import secrets
+
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+from configurations import Configuration
 
-# The tool's home dir on Toolforge. This dir is available to the application at runtime, but not during the build.
-TOOL_DATA_DIR = Path(os.environ.get('TOOL_DATA_DIR', BASE_DIR))
+class Base(Configuration):
+    # Build paths inside the project like this: BASE_DIR / 'subdir'.
+    BASE_DIR = Path(__file__).resolve().parent.parent
 
+    # The tool's home dir on Toolforge. This dir is available to the application at runtime, but not during the build.
+    TOOL_DATA_DIR = Path(os.environ.get('TOOL_DATA_DIR', BASE_DIR))
+ 
+    # Quick-start development settings - unsuitable for production
+    # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+    # Django requires a unique secret key for each Django app, that is used by several of its
+    # security features. To simplify initial setup (without hardcoding the secret in the source
+    # code) we set this to a random value every time the app starts. However, this will mean many
+    # Django features break whenever an app restarts (for example, sessions will be logged out).
+    # In your production apps you should set the `DJANGO_SECRET_KEY` env var explicitly.
+    # Make sure to use a long unique value, like you would for a password. See:
+    # https://docs.djangoproject.com/en/4.2/ref/settings/#std-setting-SECRET_KEY
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = os.environ.get(
+        "DJANGO_SECRET_KEY",
+        default=secrets.token_urlsafe(nbytes=64),
+    )
 
-# Django requires a unique secret key for each Django app, that is used by several of its
-# security features. To simplify initial setup (without hardcoding the secret in the source
-# code) we set this to a random value every time the app starts. However, this will mean many
-# Django features break whenever an app restarts (for example, sessions will be logged out).
-# In your production apps you should set the `DJANGO_SECRET_KEY` env var explicitly.
-# Make sure to use a long unique value, like you would for a password. See:
-# https://docs.djangoproject.com/en/4.2/ref/settings/#std-setting-SECRET_KEY
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    "DJANGO_SECRET_KEY",
-    default=secrets.token_urlsafe(nbytes=64),
-)
+    CSRF_TRUSTED_ORIGINS = ['https://django-test.toolforge.org']
+    # CSRF_COOKIE_SECURE = True
+    # SESSION_COOKIE_SECURE = True
 
-CSRF_TRUSTED_ORIGINS = ['https://django-test.toolforge.org']
-# CSRF_COOKIE_SECURE = True
-# SESSION_COOKIE_SECURE = True
+    # Application definition
+    INSTALLED_APPS = [
+        # Use WhiteNoise's runserver implementation instead of the Django default, for dev-prod parity.
+        'whitenoise.runserver_nostatic',
+        'polls.apps.PollsConfig',
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+    ]
 
-IN_PROD = int(os.environ.get("PROD", 0))
+    MIDDLEWARE = [
+        'django.middleware.security.SecurityMiddleware',
+        # Django doesn't support serving static assets in a production-ready way, so we use the
+        # WhiteNoise package to do so instead. The WhiteNoise middleware must be listed
+        # after Django's `SecurityMiddleware` so that security redirects are still performed.
+        # See: https://whitenoise.readthedocs.io
+        'whitenoise.middleware.WhiteNoiseMiddleware',
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ]
 
-# SECURITY WARNING: don't run with debug turned on in production!
-if not IN_PROD:
-    DEBUG = True
+    ROOT_URLCONF = 'mysite.urls'
 
-
-if IN_PROD:
-	ALLOWED_HOSTS = [
-    '.toolforge.org',
-	]
-else:
-    ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]']
-
-# Application definition
-
-INSTALLED_APPS = [
-    # Use WhiteNoise's runserver implementation instead of the Django default, for dev-prod parity.
-    'whitenoise.runserver_nostatic',
-    'polls.apps.PollsConfig',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    # Django doesn't support serving static assets in a production-ready way, so we use the
-    # WhiteNoise package to do so instead. The WhiteNoise middleware must be listed
-    # after Django's `SecurityMiddleware` so that security redirects are still performed.
-    # See: https://whitenoise.readthedocs.io
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-ROOT_URLCONF = 'mysite.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
+    TEMPLATES = [
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': [BASE_DIR / "templates"],
+            'APP_DIRS': True,
+            'OPTIONS': {
+                'context_processors': [
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',
+                    'django.contrib.messages.context_processors.messages',
+                ],
+            },
         },
-    },
-]
+    ]
 
-WSGI_APPLICATION = 'mysite.wsgi.application'
+    WSGI_APPLICATION = 'mysite.wsgi.application'
+
+    # Password validation
+    # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
+
+    AUTH_PASSWORD_VALIDATORS = [
+        {
+            'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        },
+    ]
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': TOOL_DATA_DIR / 'db.sqlite3',
-	}
+    # Internationalization
+    # https://docs.djangoproject.com/en/4.2/topics/i18n/
+    LANGUAGE_CODE = 'en-us'
+
+    TIME_ZONE = 'Europe/Paris'
+
+    USE_I18N = True
+
+    USE_TZ = True
+
+
+    # Static files (CSS, JavaScript, Images)
+    # https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+    STATIC_URL = '/static/'
+    STATIC_ROOT = BASE_DIR / 'static'
+
+    STORAGES = {
+        # Enable WhiteNoise's GZip and Brotli compression of static assets:
+        # https://whitenoise.readthedocs.io/en/latest/django.html#add-compression-and-caching-support
+        "staticfiles": {
+            "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        },
+    }
+
+    # Don't store the original (un-hashed filename) version of static files, to reduce slug size:
+    # https://whitenoise.readthedocs.io/en/latest/django.html#WHITENOISE_KEEP_ONLY_HASHED_FILES
+    WHITENOISE_KEEP_ONLY_HASHED_FILES = True
+
+
+    # Default primary key field type
+    # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+    DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+class Development(Base):
+    DEBUG = True
+    ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]']
+    # Database
+    # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': Base.TOOL_DATA_DIR / 'db.sqlite3',
+    }
+}
+
+class Production(Base):
+# SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = False
+    ALLOWED_HOSTS = ['.toolforge.org',]
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 's55463__polls',
+        "USER": os.environ.get("TOOL_TOOLSDB_USER"),
+        "PASSWORD": os.environ.get("TOOL_TOOLSDB_PASSWORD"),
+        'HOST': 'tools.db.svc.wikimedia.cloud',
+        'OPTIONS': {
+           'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+           'charset': 'utf8mb4',
+        },
+    }
 }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'Europe/Paris'
-
-USE_I18N = True
-
-USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
-
-STORAGES = {
-    # Enable WhiteNoise's GZip and Brotli compression of static assets:
-    # https://whitenoise.readthedocs.io/en/latest/django.html#add-compression-and-caching-support
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
-
-# Don't store the original (un-hashed filename) version of static files, to reduce slug size:
-# https://whitenoise.readthedocs.io/en/latest/django.html#WHITENOISE_KEEP_ONLY_HASHED_FILES
-WHITENOISE_KEEP_ONLY_HASHED_FILES = True
 
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+from configurations.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault('DJANGO_CONFIGURATION', 'Development')
 
 application = get_wsgi_application()
