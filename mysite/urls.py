66c26a04ee04e653a47f262ba0f345e067944201
Slavina Stefanova <sstefanova@wikimedia.org>
2024-05-05 10:45:28 +0300

    healthz

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from .views import healthz
 
 urlpatterns = [
     path("polls/", include("polls.urls")),
     path('admin/', admin.site.urls),
+    path('healthz/', healthz, name='healthz'),
 ]
